0616f6be0319d80af2b1631561e65cc667b094c5
coolabdulsamad <coolabdulsamad09@gmail.com>
2025-10-26 13:46:06 +0100

    add the local host to works with the backend side

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: 'https://purple-premium-bread.vercel.app',
+  origin: ['https://purple-premium-bread.vercel.app', "http://localhost:5173"],
   credentials: true // Optional: only if using cookies/sessions
 }));
 
