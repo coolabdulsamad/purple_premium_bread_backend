d0d0dffd78060275c93df8633f78a7b529de3a34
coolabdulsamad <coolabdulsamad09@gmail.com>
2025-11-09 07:49:30 +0100

    updating the project by modifiying the report page 30

--- routes/services.js
+++ routes/services.js
@@ -18,15 +18,15 @@ const authenticate = require('../middleware/authenticate'); // Use your existing
 //     }
 // });
 
-router.get('/', async (req, res) => {
-    try {
-        const result = await db.pool.query('SELECT * FROM services WHERE is_active = true');
-        res.status(200).json(result.rows);
-    } catch (error) {
-        console.error('Error fetching services:', error);
-        res.status(500).json({ error: 'Failed to fetch services.', details: error.message });
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const result = await db.pool.query('SELECT * FROM services WHERE is_active = true');
+//         res.status(200).json(result.rows);
+//     } catch (error) {
+//         console.error('Error fetching services:', error);
+//         res.status(500).json({ error: 'Failed to fetch services.', details: error.message });
+//     }
+// });
 
 // Get all services
 router.get('/', authenticate, async (req, res) => {
