5ec580eee2f27b873a5c2e703936aa59e2c43683
coolabdulsamad <coolabdulsamad09@gmail.com>
2025-11-17 18:15:10 +0100

    updating the project by modifiying the project  to implement new features like advantage 19

--- server.js
+++ server.js
@@ -45,20 +45,6 @@ app.use(cors({
     credentials: true // Optional: only if using cookies/sessions
 }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-
-    next();
-});
-
-
 app.use(express.json());
 app.use(fileUpload());
 
